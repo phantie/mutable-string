3270e10c65c420d62ff8f61247ae21dad78b9388
phantie <funtom1605@gmail.com>
2020-11-26 20:40:38 +0200

    Revert "upd deps"
    This reverts commit f08b3c2137871c6ae565179dd25bf3a906579033.

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     long_description = open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
           'take @ https://github.com/phantie/take/archive/1.0.zip',
-          'ruption @ https://github.com/phantie/ruption/archive/1.3.tar.gz',
+          'ruption @ https://github.com/phantie/ruption/archive/master.zip#egg=option-1.3',
       ]
 )
